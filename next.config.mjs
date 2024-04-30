a8d390f228a80e47e8af7bcc65f6588ea5c4aeda
ortima <account.net@.mail.ru>
2024-04-30 20:13:22 +0300

    fix config for img

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: ['rickandmortyapi.com'],
+    },
+
+};
 
 export default nextConfig;
