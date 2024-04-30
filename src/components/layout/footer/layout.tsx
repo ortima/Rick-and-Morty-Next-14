980ea76dabecf31cbb0490a252d1ddf07c52a7d1
ortima <account.net@.mail.ru>
2024-04-30 20:12:59 +0300

    css fixes

--- src/components/layout/footer/layout.tsx
+++ src/components/layout/footer/layout.tsx
@@ -1,16 +1,35 @@
 import React from "react";
+import { GoDotFill } from "react-icons/go";
+import Link from "next/link";
 
 export const FooterLayout = () => {
   return (
-    <footer className="bottom-0 left-0 fixed w-full min-h-[300px] bg-gray-500">
-      <div className="container text-slate-600">
-        <ul className="flex justify-center">
-          <li>Characters</li>
-          <li>Episodes</li>
-          <li>Locations</li>
-          <li>Server status</li>
+    <footer className="shrink-0 flex-grow-0 w-full min-h-[200px] bg-[#202329] flex justify-center">
+      <div className="container flex flex-col items-center justify-around text-neutral-400 text-sm sm:text-xl">
+        <ul className="flex gap-3.5 uppercase">
+          <li className="cursor-pointer hover:text-orange-500">Characters:</li>
+          <li className="cursor-pointer hover:text-orange-500">Episodes:</li>
+          <li className="cursor-pointer hover:text-orange-500">Locations:</li>
         </ul>
-        <p>by ortima</p>
+        <Link
+          target={"_blank"}
+          href="https://status.rickandmortyapi.com/"
+          className="flex items-center uppercase "
+        >
+          <span className="hover:text-orange-500">Server status</span>
+          <span className="text-[#6fff5b]">
+            <GoDotFill />
+          </span>
+        </Link>
+        <p>
+          ❮❯&nbsp;by&nbsp;
+          <span className="text-white underline underline-offset-4 cursor-pointer hover:text-orange-500 hover:no-underline">
+            <Link target={"_blank"} href="https://github.com/ortima">
+              ortima
+            </Link>
+          </span>
+          &nbsp;2024
+        </p>
       </div>
     </footer>
   );
--- src/components/layout/header/layout.tsx
+++ src/components/layout/header/layout.tsx
@@ -2,7 +2,7 @@ import { Logo } from "@/components/icons";
 
 export const HeaderLayout = () => {
   return (
-    <header className="bg-gray-500 w-full">
+    <header className="bg-white w-full">
       <div className="container flex justify-between items-center py-4 ">
         <Logo />
         <nav className="text-slate-600">
