4172385e794e2eed3208e82736a0b56a9e7066f1
ortima <account.net@.mail.ru>
2024-05-04 19:50:51 +0300

    fix config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['rickandmortyapi.com'],
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'rickandmortyapi.com',
+                port: '',
+                pathname: '/**',
+            },
+        ],
     },
 
 };
