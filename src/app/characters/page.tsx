55d3d9bdd285d1bf92563250c6e9d1377cff8b3d
ortima <account.net@.mail.ru>
2024-05-06 00:36:11 +0300

    csr page

--- src/app/characters/page.tsx
+++ src/app/characters/page.tsx
@@ -1,5 +1,89 @@
+"use client";
+import graphqlClient from "@/lib/client";
+import GET_CHARACTERS from "@/lib/graphql/characters";
+import { CharacterCard } from "@/components/characters";
+import React, { Suspense, useEffect, useState } from "react";
+import {
+  notFound,
+  usePathname,
+  useRouter,
+  useSearchParams,
+} from "next/navigation";
+import Pagination from "@/components/common/pagination";
+
 const Characters = () => {
-  return <div>characters</div>;
+  const [characters, setCharacters] = useState<Character[] | null>(null);
+  const [info, setInfo] = useState<Info | null>(null);
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const router = useRouter();
+  const pathname = usePathname();
+  const params = useSearchParams();
+
+  useEffect(() => {
+    graphqlClient
+      .query({ query: GET_CHARACTERS, variables: { page: currentPage } })
+      .then((response) => {
+        const { info, results } = response.data.characters;
+        setInfo(info);
+        setCharacters(results);
+      })
+      .catch((error) => {
+        console.error("Error fetching", error);
+      });
+  }, [currentPage]);
+
+  useEffect(() => {
+    const pageQueryParam = Number(params.get("page"));
+
+    if (!isNaN(pageQueryParam) && pageQueryParam > 0 && pageQueryParam < 43) {
+      setCurrentPage(pageQueryParam);
+    } else if (pathname == "/characters") {
+      router.push("/characters?page=1");
+    } else {
+      notFound();
+    }
+  }, [params, pathname, router]);
+
+  useEffect(() => {
+    const url = `${pathname}/?page=${currentPage}`;
+    router.push(url);
+  }, [currentPage]);
+
+  const handlePrevPage = () => {
+    setCurrentPage((prevPage) => prevPage - 1);
+  };
+
+  const handleNextPage = () => {
+    setCurrentPage((prevPage) => prevPage + 1);
+  };
+
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <div className="container">
+        <div className="sm:flex sm:flex-row md:text-2xl sm:justify-between sm:text-white sm:pb-16 flex flex-col text-white items-center pb-8 gap-5 text-base">
+          <p>
+            Shown: {characters?.length} of {info?.count}{" "}
+          </p>
+          <p>Current page: {currentPage}</p>
+          <p>Total pages: {info && info.pages}</p>
+        </div>
+        <ul className="grid gap-3.5 md:gap-y-7 grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+          {characters &&
+            characters.map((character: Character) => (
+              <CharacterCard character={character} key={character.id} />
+            ))}
+        </ul>
+        <Pagination
+          currentPage={currentPage}
+          totalPages={info?.pages}
+          nextPageNumber={info?.next}
+          prevPageNumber={info?.prev}
+          onNextPage={handleNextPage}
+          onPrevPage={handlePrevPage}
+        />
+      </div>
+    </Suspense>
+  );
 };
 
 export default Characters;
