85b5843f0591fdeb81ce070c69771def1ed28b02
ortima <account.net@.mail.ru>
2024-05-06 00:33:38 +0300

    footer data

--- src/components/layout/footer/layout.tsx
+++ src/components/layout/footer/layout.tsx
@@ -1,36 +1,71 @@
 import React from "react";
 import { GoDotFill } from "react-icons/go";
 import Link from "next/link";
+import graphqlClient from "@/lib/client";
+import GET_INFO from "@/lib/graphql/info";
 
-export const FooterLayout = () => {
-  return (
-    <footer className="shrink-0 flex-grow-0 w-full min-h-[200px] bg-[#202329] flex justify-center">
-      <div className="container flex flex-col items-center justify-around text-neutral-400 text-sm sm:text-xl">
-        <ul className="flex gap-3.5 uppercase">
-          <li className="cursor-pointer hover:text-orange-500">Characters:</li>
-          <li className="cursor-pointer hover:text-orange-500">Episodes:</li>
-          <li className="cursor-pointer hover:text-orange-500">Locations:</li>
-        </ul>
-        <Link
-          target={"_blank"}
-          href="https://status.rickandmortyapi.com/"
-          className="flex items-center uppercase "
-        >
-          <span className="hover:text-orange-500">Server status</span>
-          <span className="text-[#6fff5b]">
-            <GoDotFill />
-          </span>
-        </Link>
-        <p>
-          ❮❯&nbsp;by&nbsp;
-          <span className="text-white underline underline-offset-4 cursor-pointer hover:text-orange-500 hover:no-underline">
-            <Link target={"_blank"} href="https://github.com/ortima">
-              ortima
-            </Link>
-          </span>
-          &nbsp;2024
-        </p>
-      </div>
-    </footer>
-  );
+type Info = {
+  count: number;
+};
+interface CharacterData {
+  info: Info;
+}
+interface EpisodeData {
+  info: Info;
+}
+interface LocationData {
+  info: Info;
+}
+interface QueryData {
+  characters: CharacterData;
+  episodes: EpisodeData;
+  locations: LocationData;
+}
+export const FooterLayout: React.FC = async () => {
+  try {
+    const { data } = await graphqlClient.query<QueryData>({
+      query: GET_INFO,
+    });
+    const { characters, episodes, locations } = data;
+
+    return (
+      <footer className="shrink-0 flex-grow-0 w-full min-h-[300px] bg-[#202329] flex justify-center">
+        <div className="container flex flex-col py-6 items-center justify-around text-neutral-400 text-sm sm:text-xl">
+          <ul className="flex gap-3.5 uppercase max-[425px]:grid">
+            <li className="cursor-pointer hover:text-orange-500">
+              Characters: {characters.info.count}
+            </li>
+            <li className="cursor-pointer hover:text-orange-500">
+              Episodes: {episodes.info.count}
+            </li>
+            <li className="cursor-pointer hover:text-orange-500">
+              Locations: {locations.info.count}
+            </li>
+          </ul>
+          <Link
+            target={"_blank"}
+            href="https://status.rickandmortyapi.com/"
+            className="flex items-center uppercase"
+          >
+            <span className="hover:text-orange-500">Server status</span>
+            <span className="text-[#6fff5b]">
+              <GoDotFill />
+            </span>
+          </Link>
+          <p>
+            ❮❯&nbsp;by&nbsp;
+            <span className="text-white underline underline-offset-4 cursor-pointer hover:text-orange-500 hover:no-underline">
+              <Link target={"_blank"} href="https://github.com/ortima">
+                ortima
+              </Link>
+            </span>
+            &nbsp;2024
+          </p>
+        </div>
+      </footer>
+    );
+  } catch (e) {
+    console.error(e);
+    return null;
+  }
 };
