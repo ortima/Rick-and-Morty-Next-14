9b1fe91a08a948fb0f97ff995a9223aaa424cbb9
ortima <account.net@.mail.ru>
2024-05-11 00:23:14 +0300

    loader indicator

--- src/app/characters/page.tsx
+++ src/app/characters/page.tsx
@@ -1,8 +1,9 @@
 "use client";
+import { useState, useEffect } from "react";
+import { CharacterCard } from "@/components/characters";
+import { PacmanLoader } from "react-spinners";
 import graphqlClient from "@/lib/client";
 import GET_CHARACTERS from "@/lib/graphql/characters";
-import { CharacterCard } from "@/components/characters";
-import React, { useEffect, useState } from "react";
 import {
   notFound,
   usePathname,
@@ -16,20 +17,24 @@ const Characters = () => {
   const [characters, setCharacters] = useState<Character[] | null>(null);
   const [info, setInfo] = useState<Info | null>(null);
   const [currentPage, setCurrentPage] = useState<number>(1);
+  const [loading, setLoading] = useState<boolean>(false); // Добавлено состояние загрузки
   const router = useRouter();
   const pathname = usePathname();
   const params = useSearchParams();
 
   useEffect(() => {
+    setLoading(true); // Устанавливаем состояние загрузки в true при начале загрузки данных
     graphqlClient
       .query({ query: GET_CHARACTERS, variables: { page: currentPage } })
       .then((response) => {
         const { info, results } = response.data.characters;
         setInfo(info);
         setCharacters(results);
+        setLoading(false); // Устанавливаем состояние загрузки в false после загрузки данных
       })
       .catch((error) => {
         console.error("Error fetching", error);
+        setLoading(false); // Устанавливаем состояние загрузки в false в случае ошибки загрузки данных
       });
   }, [currentPage]);
 
@@ -62,25 +67,35 @@ const Characters = () => {
     <div className="container">
       <div className="sm:flex sm:flex-row md:text-2xl sm:justify-between sm:text-white sm:pb-16 flex flex-col text-white items-center pb-8 gap-5 text-base">
         <p>
-          Shown: {characters?.length} of {info?.count}{" "}
+          Shown: {loading ? "Loading..." : characters?.length} of {info?.count}{" "}
         </p>
         <p>Current page: {currentPage}</p>
         <p>Total pages: {info && info.pages}</p>
       </div>
-      <ul className="grid gap-3.5 md:gap-y-7 grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-        {characters &&
-          characters.map((character: Character) => (
-            <CharacterCard character={character} key={character.id} />
-          ))}
-      </ul>
-      <Pagination
-        currentPage={currentPage}
-        totalPages={info?.pages}
-        nextPageNumber={info?.next}
-        prevPageNumber={info?.prev}
-        onNextPage={handleNextPage}
-        onPrevPage={handlePrevPage}
-      />
+      {loading ? (
+        <div className="flex justify-center items-center">
+          <PacmanLoader color="rgba(231, 107, 28, 1)" size={50} />
+        </div>
+      ) : (
+        <ul className="grid gap-3.5 md:gap-y-7 grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+          {characters &&
+            characters.map((character: Character) => (
+              <CharacterCard character={character} key={character.id} />
+            ))}
+        </ul>
+      )}
+      {loading ? (
+        <div></div>
+      ) : (
+        <Pagination
+          currentPage={currentPage}
+          totalPages={info?.pages}
+          nextPageNumber={info?.next}
+          prevPageNumber={info?.prev}
+          onNextPage={handleNextPage}
+          onPrevPage={handlePrevPage}
+        />
+      )}
     </div>
   );
 };
