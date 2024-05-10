f574e8a991264a83ed140d688f64b78adad062f7
ortima <account.net@.mail.ru>
2024-05-11 00:21:47 +0300

    loader pacman

--- src/app/characters/loading.tsx
+++ src/app/characters/loading.tsx
@@ -1,7 +1,15 @@
-import React from "react";
+import { PacmanLoader } from "react-spinners";
 
 const Loading = () => {
-  return <div>loading...</div>;
+  return (
+    <section className="py-24">
+      <div className="container">
+        <div className="mx-auto">
+          <PacmanLoader color="rgba(231, 107, 28, 1)" size={50} />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Loading;
